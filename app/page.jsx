c737fdf23703ebc90f2a84fd40e641cec816d721
Jinad Tope Isaac <jinadtope66@gmail.com>
2024-10-25 01:12:03 +0100

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -28,7 +28,7 @@ export default function Home() {
             {/* Left Side - Text and CTA */}
             <div className="space-y-4">
               <h1 className="text-4xl font-bold text-gray-900">
-                About Divi Tutoring
+                About Maxfax Tutorials
               </h1>
               <p className="text-gray-600" data-aos="fade-up">
                 At Divi Tutoring, we provide top-notch educational support for
